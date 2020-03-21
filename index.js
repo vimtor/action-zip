a136662816efb708485d79d7890f009dc4394e97
papeloto <vn4varro@gmail.com>
2020-03-21 12:41:59 +0100

    feat: project files

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+const AdmZip = require("adm-zip");
+const core = require("@actions/core");
+
+const inputFiles = core.getInput("files", { required: true });
+const filename = core.getInput("filename");
+
+const files = inputFiles.split(" ");
+
+console.log(`Ready to zip "${inputFiles}" into ${filename}`);
+
+const zip = new AdmZip();
+
+files.forEach(file => {
+  if (!fs.existsSync(file)) {
+    console.log(`  - ${file} (Not Found)`);
+    return;
+  }
+
+  const dir = path.dirname(file);
+  const stats = fs.lstatSync(file);
+
+  if (stats.isDirectory()) {
+    zip.addLocalFolder(file, dir);
+  } else {
+    zip.addLocalFile(file, dir === "." ? "" : dir);
+  }
+
+  console.log(`  - ${file}`);
+});
+
+zip.writeZip(filename);
+
+console.log(`\nZipped file ${filename}`);
+
+core.setOutput(filename);
